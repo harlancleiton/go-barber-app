126b875a4198fbae02b85e0a98683416170fed12
Harlan Cleiton <harlancleiton@gmail.com>
2019-11-11 22:33:34 -0300

    feat: Add Button and Input components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '../../components/Background';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn = () => {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input icon="person" placeholder="Digite seu nome" />
+
+      <Button>Entrar</Button>
     </Background>
   );
 };
